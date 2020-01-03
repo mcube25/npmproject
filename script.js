a93a1f7ac3b9fcb0d4f13453b63cc359ca1cc7bb
mcube3567 <mcube3567@gmail.com>
2020-01-03 22:50:04 +0100

    analogy

---
+++ script.js
@@ -0,0 +1,25 @@
+var _ = require('lodash');
+var array = [1,2,3,4,5,6,];
+/*lodash method*/          console.log('answer', _.without(array,3));
+3. In Git bash type:
+/written codes by a community
+//search for packages in d npm registry
+//packages contain 2 tings 1. a js file 2.package  json file
+//package.json file is a minifile dat describes d package
+ /*setting up npm and package json
+ node allos us run javascript outside d browser
+ npm -v // npm version
+ node -v //node version
+ npm init //initializes npm
+ react file lives in npm
+ live-server
+ installing it -g
+ npm install -g live-server
+ sudo npm install -g live-server
+ lodash
+ npm install lodash
+ npm install browserify
+ //hatchful free logo maker
+
+
+ */
