163083ab9fca0128ae77ed6feeb41ba3731a056d
mcube3567 <mcube3567@gmail.com>
2020-01-05 22:55:18 +0100

    react app

--- react.js
+++ react.js
@@ -16,3 +16,58 @@ ReactDOM.render(<App/>document.getElementById('root'));
 registerServiceWorker();
 
 */
+//building a react app
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//we can change the fnction into a class
+//the function 
+function App(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+//becomes
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+ 
+export default App;
+
